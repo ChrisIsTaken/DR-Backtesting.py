8d44c544ff3178099b653d442d60ff6b53aa837f
ChrisIsTaken <s4004634@ba-sachsen.de>
2023-01-31 12:58:15 +0100

    Still trying to get the output to work considering the levelbreaks issues.

--- bt.py
+++ bt.py
@@ -16,7 +16,7 @@ data['minute'] = data['timestamp'].dt.minute
 #define breaklevel function
 def breaklevel(open_price, close_price, level):
     print("breaklevel function has been initiated")
-    print("current level: ", level)
+    print("current level ", level)
     print("current open: ", open_price)
     print("current close: ", close_price)
     result = None
@@ -50,7 +50,7 @@ class MyStrategy(Strategy):
         self.odr_session_vars = {
             'session_name': 'ODR',
             'defining_hour_start': "03:00",
-            'defining_hour_end': "04:30",
+            'defining_hour_end': "04:00",
             'session_validity': "08:30",
             'dr_high': None,
             'dr_high_timestamp': None,
@@ -78,6 +78,7 @@ class MyStrategy(Strategy):
             'idr_low_timestamp': None,
             'levelbreaks': [],
         }
+
     def next(self):
         print("Reached next(self)")
         for sessions in [self.rdr_session_vars, self.odr_session_vars, self.adr_session_vars]:
@@ -86,6 +87,7 @@ class MyStrategy(Strategy):
             print("_______________________________________")
             # Your strategy logic here
             #Checking if current time is in between defining hour
+            print("last time: ", self.data.Time[-1])
             if (self.data.Time[-1] >= sessions['defining_hour_start']) and (self.data.Time[-1] < sessions['defining_hour_end']):
                 print("session's defining hour is ongoing")
                 #Update levels
@@ -105,7 +107,9 @@ class MyStrategy(Strategy):
                 print("Updated values: drhigh:", sessions['dr_high'], "drlow: ", sessions['dr_low'], "idrhigh: ", sessions['idr_high'], "idrlow: ", sessions['idr_low'])
             else:
                 #Check if session is still valid
+                print("Defining hour passed, checking if ", sessions['session_name'], "-session is still valid")
                 if self.data.Time[-1] > sessions['defining_hour_end'] and self.data.Time[-1] <= sessions['session_validity']:
+                    print("session is valid")
                     levels = [sessions['dr_low'], sessions['idr_low'], sessions['dr_high'], sessions['idr_high']]
                     #Check if any of the sessions is none
                     #If levels are none, theres no point in running following code
@@ -121,21 +125,22 @@ class MyStrategy(Strategy):
                             levelname = None
                             if level == sessions['dr_low']: levelname = 'dr_low'
                             if level == sessions['dr_high']: levelname = 'dr_high'
-                            if level == sessions['idr_low']: levelname = 'idr_high'
-                            if level == sessions['idr_high']: levelname = 'idr_low'
+                            if level == sessions['idr_low']: levelname = 'idr_low'
+                            if level == sessions['idr_high']: levelname = 'idr_high'
                             result = breaklevel(open_price, close_price, level)
                             if result == 1 or result == 2:
                                 print("adding the following to levelbreaks: ", self.data.Date[-1], self.data.Time[-1], levelname, level, result, open_price, close_price)
-                                for item in [self.data.Date[-1], self.data.Time[-1], levelname, level, result, open_price, close_price]:
-                                    sessions['levelbreaks'].append(item)
-                        if self.data.Time[-1] == sessions['session_validity']:
-                            print("Adding following values to csv: ", [sessions['session_name'], sessions['dr_high'], sessions['dr_high_timestamp'], sessions['dr_low'], sessions['dr_low_timestamp'], sessions['idr_high'], sessions['idr_high_timestamp'], sessions['idr_low'], sessions['idr_low_timestamp'], "|", sessions['levelbreaks'], "|"])
-
-                            exportlist = [sessions['session_name'], sessions['dr_high'], sessions['dr_high_timestamp'], sessions['dr_low'], sessions['dr_low_timestamp'], sessions['idr_high'], sessions['idr_high_timestamp'], sessions['idr_low'], sessions['idr_low_timestamp'], "|", sessions['levelbreaks'], "|"]
-                            df = pd.DataFrame(exportlist)
-                            df.to_csv('session_results.csv',sep=str(','))
-                #else:
-                    #session is not valid anymore, append values to csv
+                                
+                                #for item in [self.data.Date[-1], self.data.Time[-1], levelname, level, result, open_price, close_price]:
+                                sessions['levelbreaks'].append([self.data.Date[-1], self.data.Time[-1], levelname, level, result, open_price, close_price])
+                                    
+                    if self.data.Time[-1] == sessions['session_validity']:
+                        print("add to csv")
+                        df = pd.DataFrame(sessions)
+                        df.to_csv('session_results.csv',sep=str(','))
+                        print("clearing levelbreaks")
+                        sessions['levelbreaks'].clear
+                            
 # Create a Backtest object using the data and the strategy
 bt = Backtest(data, MyStrategy, cash=100000, commission=.002)
 stats = bt.run()
